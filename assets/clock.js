dc3a404a5ae802edcf847780be84724cf29c79f8
Will Dev <rafaelmelo75831@gmail.com>
2024-08-04 23:04:19 -0300

    added const and document query about times

--- assets/clock.js
+++ assets/clock.js
@@ -1,4 +1,7 @@
-
+//Capturar nossos ponteiros para realizarmos funcoes
+const hourHand = document.querySelector('.hour-hand'); 
+const minuteHand = document.querySelector('.minute-hand');
+const secondHand = document.querySelector('.second-hand');
 
 const getTime = () => { //Criando função para buscar horario atual 
     const date = new Date(); //criando objeto DATE
